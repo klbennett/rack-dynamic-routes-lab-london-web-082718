06417f7b35bfe4ee3db398e2a911370322c61a1e
Kay B <kaylouisebennett@gmail.com>
2018-09-16 08:31:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,10 +8,12 @@ class Application
       new_item = req.path.split("/items/").last #turn /items/itemname into itemname
          if @@items.find{|item| item.name == new_item}
             resp.write found_item.price
+          end
       else
           resp.write "Item not found"
           resp.status = 400
       end
+      
     else
       resp.write "Route not found"
       resp.status = 404
